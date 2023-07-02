4653643bd9d41db36490d3db8c47661822a591b1
Franco Dugo Vidal <Franco.Dugo.Vidal@ey.com>
2023-07-02 19:57:50 -0300

    Domain Layer: Adding Primitives (DomainEvents, AggregateRoot and IUnitOfWork

---
+++ ddd-cleanarch-api/Domain/Primitives/AggregateRoot.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain.Primitives;
+
+public abstract class AggregateRoot
+{
+    /// <summary>
+    ///  Domain Events list.
+    /// </summary>
+    private readonly List<DomainEvent> _domainEvents = new();
+
+    /// <summary>
+    /// Getting all the Domain Events within this Aggregate.
+    /// </summary>
+    /// <returns>List<DomainEvents></returns>
+    public ICollection<DomainEvent> GetDomainEvents()
+        => _domainEvents;
+
+    /// <summary>
+    ///  Raise a Domain Event.
+    /// </summary>
+    protected void Raise(DomainEvent domainEvent)
+    { 
+        _domainEvents.Add(domainEvent);
+    }
+}
